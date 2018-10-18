aadbbe668ddac844b586d0f66131c3bb8369d2b5
James Hunt <jhunt@inmotionsoftware.com>
2018-10-17 21:30:24 -0500

    paidUseCase uses actual prices if one of the currencies is the same as the base currency

--- business/src/main/java/me/jameshunt/business/PaidUseCase.kt
+++ business/src/main/java/me/jameshunt/business/PaidUseCase.kt
@@ -6,26 +6,29 @@ import javax.inject.Inject
 
 class PaidUseCase @Inject constructor(
         private val sortTransactionUseCase: SortTransactionUseCase,
-        private val exchangeRateUseCase: ExchangeRateUseCase
+        private val exchangeRateUseCase: ExchangeRateUseCase,
+        private val selectedCurrencyUseCase: SelectedCurrencyUseCase
 ) {
 
     fun getPaidForCurrentlyHeld(currencyType: CurrencyType): Observable<DataSource<Double>> {
         return sortTransactionUseCase.getSortedTransactions(currencyType = currencyType)
                 .map { dataSource ->
                     dataSource.mapSuccess { sortedTransactions ->
+
                         val purchasedAmount = sortedTransactions.purchased
                                 .asSequence()
-
-                                //todo: use transaction values if already in the correct base
                                 .map { purchase ->
-                                    exchangeRateUseCase
-                                            .getExchangeRateAtTime(
-                                                    target = purchase.toCurrencyType,
-                                                    unixMilliSeconds = purchase.time,
-                                                    exchangeType = purchase.exchangeType
-                                            )
-                                            .blockingGet()
-                                            .mapSuccess { it * purchase.toAmount }
+                                    when (selectedCurrencyUseCase.selectedBase == purchase.fromCurrencyType) {
+                                        true -> DataSource.Success(purchase.fromAmount)
+                                        false -> exchangeRateUseCase
+                                                .getExchangeRateAtTime(
+                                                        target = purchase.toCurrencyType,
+                                                        unixMilliSeconds = purchase.time,
+                                                        exchangeType = purchase.exchangeType
+                                                )
+                                                .blockingGet()
+                                                .mapSuccess { it * purchase.toAmount }
+                                    }
                                 }
                                 .fold(DataSource.Success(0.0) as DataSource<Double>) { acc, dataSource ->
                                     acc.join(dataSource) { a, b -> a + b }
@@ -33,17 +36,18 @@ class PaidUseCase @Inject constructor(
 
                         val soldAmount = sortedTransactions.sold
                                 .asSequence()
-
-                                //todo: use transaction values if already in the correct base
                                 .map { sold ->
-                                    exchangeRateUseCase
-                                            .getExchangeRateAtTime(
-                                                    target = sold.fromCurrencyType,
-                                                    unixMilliSeconds = sold.time,
-                                                    exchangeType = sold.exchangeType
-                                            )
-                                            .blockingGet()
-                                            .mapSuccess { it * sold.fromAmount }
+                                    when (selectedCurrencyUseCase.selectedBase == sold.toCurrencyType) {
+                                        true -> DataSource.Success(sold.toAmount)
+                                        false -> exchangeRateUseCase
+                                                .getExchangeRateAtTime(
+                                                        target = sold.fromCurrencyType,
+                                                        unixMilliSeconds = sold.time,
+                                                        exchangeType = sold.exchangeType
+                                                )
+                                                .blockingGet()
+                                                .mapSuccess { it * sold.fromAmount }
+                                    }
                                 }
                                 .fold(DataSource.Success(0.0) as DataSource<Double>) { acc, dataSource ->
                                     acc.join(dataSource) { a, b -> a + b }
